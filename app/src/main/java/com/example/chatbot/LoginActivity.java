0f1f8af62d75934a1498afed37ea7f76fb8c5685
b0ink <40929320+b0ink@users.noreply.github.com>
2024-05-06 14:44:21 +1000

    Init login activity

---
+++ app/src/main/java/com/example/chatbot/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.example.chatbot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatbot.API.RetrofitClient;
+import com.example.chatbot.API.models.ResponsePost;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private Button btnStartTalking;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etUsername = findViewById(R.id.etUsername);
+        btnStartTalking = findViewById(R.id.btnStartTalking);
+
+        btnStartTalking.setOnClickListener(view -> {
+            String username = etUsername.getText().toString().trim();
+            //TODO check for white space
+
+            Call<ResponsePost> call = RetrofitClient.getInstance()
+                    .getAPI().initUser(username);
+
+            call.enqueue(new Callback<ResponsePost>() {
+                @Override
+                public void onResponse(Call<ResponsePost> call, Response<ResponsePost> response) {
+                    if (!response.isSuccessful()) {
+                        System.out.println("Error occurred!");
+                        Toast.makeText(LoginActivity.this, "An error occurred.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra(MainActivity.EXTRA_USERS_NAME, username);
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onFailure(Call<ResponsePost> call, Throwable throwable) {
+                    System.out.println("InitUser error:" + throwable.getMessage());
+                }
+            });
+        });
+    }
+}
--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chatbot;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +9,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.chatbot.API.RetrofitClient;
+import com.example.chatbot.API.models.ResponsePost;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_USERS_NAME = "extra_users_name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        Intent intent = getIntent();
+        if (intent == null || !intent.hasExtra(EXTRA_USERS_NAME)) {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+            return;
+        }
+
+        String username = intent.getStringExtra(EXTRA_USERS_NAME);
+        String message = "todo";
+
+        Call<ResponsePost> call = RetrofitClient.getInstance()
+                .getAPI().getChatResponse(username, message);
+
+        call.enqueue(new Callback<ResponsePost>() {
+            @Override
+            public void onResponse(Call<ResponsePost> call, Response<ResponsePost> response) {
+                if (!response.isSuccessful()) {
+                    System.out.println("Error occurred!");
+                    return;
+                }
+
+                String messageResponse = response.body().message;
+
+
+//                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<ResponsePost> call, Throwable throwable) {
+                System.out.println("BAD ERROR" + throwable.getMessage());
+            }
+        });
+
     }
 }
