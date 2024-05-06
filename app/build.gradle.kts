673aca2efdb9dfc27965e73b99c9419c366b060c
b0ink <40929320+b0ink@users.noreply.github.com>
2024-05-06 14:30:28 +1000

    Add retrofit API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,9 @@ android {
 }
 
 dependencies {
-
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
---
+++ app/src/main/java/com/example/chatbot/API/API.java
@@ -0,0 +1,28 @@
+package com.example.chatbot.API;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import com.example.chatbot.API.models.ResponsePost;
+
+public interface API {
+    // Initiate and save new conversation server-sided
+    @FormUrlEncoded
+    @POST("/verify-username")
+    Call<ResponsePost> initUser(
+            @Field("username") String username
+    );
+
+    @FormUrlEncoded
+    @POST("/chat")
+    Call<ResponsePost> getChatResponse(
+            @Field("username") String username,
+            @Field("username") String message
+    );
+}
